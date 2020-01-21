6a198f60dc870b8cf9b1126857179681b9794b4c
Esteban <esteb.gtz.mtz@hotmail.com>
2020-01-21 12:23:12 -0600

    add a enemy
    Signed-off-by: Esteban <esteb.gtz.mtz@hotmail.com>

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
+
+const obstacles = [];
 let interval;
 let frames = 0;
 
 const imgs = {
     background: './images/background.png',
-    sprite: './images/sprite.png'
+    sprite: './images/sprite.png',
+    logo: './images/logo.png',
+    enemy: './images/enemy.png'
+
 }
 
 class Background {
@@ -22,12 +27,39 @@ class Background {
     }
     draw() {
         ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+        //ctx.drawImage(this.img, this.x + this.width, this.y, this.width, this.height);
+        ctx.drawImage(this.img, this.x - this.width, this.y, this.width, this.height);
+    }
+    goRight() {
+            this.x += 10;
+            if (this.x > canvas.width) this.x = 0;
+        }
+        // goLeft() {
+        //     this.x -= 10;
+        //     if (this.x < -canvas.width) this.x = 0;
+        // }
+}
+
+class Logo {
+    constructor() {
+        this.x = 10;
+        this.y = 10;
+        this.width = 400;
+        this.height = 150;
+        this.img = new Image();
+        this.img.src = imgs.logo;
+        this.img.onload = () => {
+            this.draw();
+        }
+    }
+    draw() {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
 }
 
 class Hero {
     constructor() {
-        this.x = (canvas.width / 2) - 80;
+        this.x = canvas.width - 450;
         this.y = canvas.height - 220;
         this.width = 120;
         this.height = 100;
@@ -44,22 +76,61 @@ class Hero {
         ctx.drawImage(this.img, this.sx, this.sy, 500, 500, this.x, this.y, this.width, this.height)
     }
     goLeft() {
-        this.x -= 5;
-        this.move();
-    }
+            this.x += .05;
+            this.move();
+
+        }
+        // goRight() {
+        //     this.x += .05;
+        //     this.move;
+        // }
     move() {
         this.sx -= 500;
     }
 }
 
+class Enemy {
+    constructor() {
+        this.x = 20;
+        this.y = canvas.height - 250;
+        this.width = 100;
+        this.height = 120;
+        this.sx = 0;
+        this.sy = 0;
+        this.img = new Image();
+        this.img.src = imgs.enemy;
+        this.img.onload = () => {
+            this.draw();
+        }
+    }
+    draw() {
+        if (this.sx > 3600) this.sx = 0;
+        ctx.drawImage(this.img, this.sx, this.sy, 450, 627, this.x, this.y, this.width, this.height)
+    }
+    goRight() {
+        this.x += 10;
+        this.move();
+    }
+    move() {
+        this.sx += 450;
+    }
+}
+
 const background = new Background();
 const hero = new Hero();
+const logo = new Logo();
+const enemy = new Enemy();
 
 const update = () => {
     frames++;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     background.draw();
+    background.goRight();
+    logo.draw();
     hero.draw();
+    hero.goLeft();
+    enemy.draw();
+    enemy.goRight();
 }
 
 
@@ -69,13 +140,17 @@ window.onload = function() {
     document.addEventListener('keydown', ({ keyCode }) => {
         switch (keyCode) {
             case 37:
-                return hero.goLeft();
+                break;
+                // case 39:
+                //     background.goLeft();
+                //     hero.goLeft();
+                //     break;
         }
     })
 
 
     const startGame = () => {
-        interval = setInterval(update, 300 / 15);
+        interval = setInterval(update, 1000 / 20);
     }
     document.getElementById("start-button").onclick = function() {
         startGame();
